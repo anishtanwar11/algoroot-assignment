a57c331b7c610d8de45a67284c275ad18df535b6
Anish <anishtanvar@gmail.com>
2025-03-27 12:26:46 +0530

    Add vercel.json files

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://algoroot-assignment-anish.vercel.app",
+    ],
     methods: ["GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   })
